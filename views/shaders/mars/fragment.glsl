f0567b87c5863bec9cfa24da30567717d22b7988
AlbertZeb10 <108046490+AlbertZeb10@users.noreply.github.com>
2022-10-30 21:38:00 +0700

    Albert

--- views/shaders/mars/fragment.glsl
+++ views/shaders/mars/fragment.glsl
@@ -5,6 +5,6 @@ varying vec3 vertexNormal;
 
 void main() {
     float intentsity = 1.05 - dot(vertexNormal, vec3(0.0, 0.0, 1.0)); 
-    vec3 atmosphere = vec3(0.82,0.41,0.12) * pow(intentsity, 1.3);
+    vec3 atmosphere = vec3(0.82,0.41,0.12) * pow(intentsity, 1.5);
     csm_DiffuseColor = vec4(atmosphere + texture2D(Texture, vertexUV).xyz, 1.0);
 }
