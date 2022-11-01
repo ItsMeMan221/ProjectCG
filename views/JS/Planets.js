4eb6cea96e28575e18fc488ec1e096a031b0e400
Santiago <igosantiago91@gmail.com>
2022-11-01 16:24:26 +0700

    shorten file  & animate all planets

--- views/JS/Planets.js
+++ views/JS/Planets.js
@@ -11,14 +11,16 @@ import { dirLight, ambLight, pntLight, hempLight } from "../utils/light";
 
 //Import all components
 import { sun } from "../components/sun";
-import { mercury } from "../components/mercury";
-import { venus } from "../components/venus";
-import { earth } from "../components/earth";
-import { mars } from "../components/mars";
-import { jupiter } from "../components/jupiter";
-import { saturn } from "../components/saturn";
-import { uranus } from "../components/uranus";
-import { neptune } from "../components/neptune";
+import {
+  mercury,
+  venus,
+  earth,
+  mars,
+  jupiter,
+  saturn,
+  uranus,
+  neptune,
+} from "../components/ExoPlanets";
 import { stars } from "../components/starPlanets";
 
 const rotationSpeed = 0.09;
@@ -26,10 +28,13 @@ let clock = new THREE.Clock();
 let coorPlanet = new THREE.Vector3();
 let coorCamera = new THREE.Vector3();
 
-earth.rotation.x = (13 / 100) * Math.PI;
+//Angle of planets
 venus.rotation.x = (4.8 / 100) * Math.PI;
+earth.rotation.x = (13 / 100) * Math.PI;
+mars.rotation.x = (14 / 100) * Math.PI;
+jupiter.rotation.x = (1.75 / 100) * Math.PI;
 saturn.rotation.x = (14.8 / 100) * Math.PI;
-uranus.rotation.x = (15.3 / 100) * Math.PI;
+uranus.rotation.x = (54.33 / 100) * Math.PI;
 neptune.rotation.x = (15.75 / 100) * Math.PI;
 // Init for interaction manager
 const interactionManager = new InteractionManager(
@@ -47,20 +52,23 @@ camera.position.x = 200;
 dirLight.position.x = -900;
 pntLight.position.x = -900;
 
+// Object Position
 sun.position.x = -900;
 mercury.position.x = -400;
 venus.position.x = -200;
 earth.position.x = 0;
 mars.position.x = 200;
-jupiter.position.x = 500;
-saturn.position.x = 1000;
-uranus.position.x = 1500;
-neptune.position.x = 1900;
+jupiter.position.x = 450;
+saturn.position.x = 800;
+uranus.position.x = 1100;
+neptune.position.x = 1300;
+
 // Adding mesh to interaction manager
 interactionManager.add(sun);
 interactionManager.add(mercury);
 interactionManager.add(venus);
 interactionManager.add(earth);
+
 // Adding all utils and components
 scene.add(
   pntLight,
@@ -83,11 +91,16 @@ function animate() {
   coorCamera = coorObj(camera);
   let delta = clock.getDelta();
   requestAnimationFrame(animate);
+
+  //Rotation Speed
   mercury.rotation.y += (rotationSpeed * delta) / 0.5;
-  venus.rotation.y += (rotationSpeed * delta) / 1;
+  venus.rotation.y += (rotationSpeed * delta) / 4;
   earth.rotation.y += rotationSpeed * delta * 3;
-  saturn.children[0].rotation.y += rotationSpeed * delta * 6.5;
-  neptune.rotation.y += rotationSpeed * delta * 4.5;
+  mars.rotation.y += rotationSpeed * delta * 3.1;
+  jupiter.rotation.y += rotationSpeed * delta * 6.3;
+  saturn.children[0].rotation.y += rotationSpeed * delta * 6;
+  uranus.rotation.y += rotationSpeed * delta * 4.5;
+  neptune.rotation.y += rotationSpeed * delta * 3.5;
 
   // update controls and interaction manager
   controls.update();
---
+++ views/components/ExoPlanets.js
@@ -0,0 +1,212 @@
+import * as THREE from "three";
+import CustomShaderMaterial from "three-custom-shader-material/vanilla";
+
+//Import Shaders
+import vertexShader from "../shaders/vertex.glsl";
+import fragmentShader from "../shaders/fragment.glsl";
+
+// all of planets
+
+// Shape of mercury
+const mercuryShape = new THREE.SphereGeometry(30, 60, 60);
+//mercury Texture
+const mercuryTexture = new THREE.TextureLoader().load(
+  "../../assets/Texture/mercury.jpg"
+);
+// mercury Material
+const mercuryMaterial = new CustomShaderMaterial({
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    instColor: {
+      value: new THREE.Vector3(0.3, 0.3, 0.3),
+    },
+    Texture: {
+      value: mercuryTexture,
+    },
+  },
+});
+
+// Shape of venus
+const venusShape = new THREE.SphereGeometry(36, 60, 60);
+//Venus Texture
+const venusTexture = new THREE.TextureLoader().load(
+  "../../assets/Texture/venus.jpg"
+);
+// Venus Material
+const venusMaterial = new CustomShaderMaterial({
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    instColor: {
+      value: new THREE.Vector3(0.8, 0.4, 0),
+    },
+    Texture: {
+      value: venusTexture,
+    },
+  },
+});
+
+// Shape of earth
+const earthShape = new THREE.SphereGeometry(37.9, 65, 65);
+//earth Texture
+const earthTexture = new THREE.TextureLoader().load(
+  "../../assets/Texture/globe.jpg"
+);
+// earth Material
+const earthMaterial = new CustomShaderMaterial({
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    instColor: {
+      value: new THREE.Vector3(0.2, 0.3, 0.8),
+    },
+    Texture: {
+      value: earthTexture,
+    },
+  },
+});
+
+// Shape of mars
+const marsShape = new THREE.SphereGeometry(32, 105, 30);
+//mars Texture
+const marsTexture = new THREE.TextureLoader().load(
+  "../../assets/Texture/mars.jpg"
+);
+// mars Material
+const marsMaterial = new CustomShaderMaterial({
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    instColor: {
+      value: new THREE.Vector3(0.82, 0.41, 0.12),
+    },
+    Texture: {
+      value: marsTexture,
+    },
+  },
+});
+
+// Shape of jupiter
+const jupiterShape = new THREE.SphereGeometry(80, 89, 97);
+//jupiter Texture
+const jupiterTexture = new THREE.TextureLoader().load(
+  "../../assets/Texture/jupiter.jpg"
+);
+// jupiter Material
+const jupiterMaterial = new CustomShaderMaterial({
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    instColor: {
+      value: new THREE.Vector3(0.84, 0.79, 0.615),
+    },
+    Texture: {
+      value: jupiterTexture,
+    },
+  },
+});
+
+// Shape of saturn
+const saturnShape = new THREE.SphereGeometry(70, 65, 65);
+//saturn Texture
+const saturnTexture = new THREE.TextureLoader().load(
+  "../../assets/Texture/saturn.jpg"
+);
+// saturn Material
+const saturnMaterial = new CustomShaderMaterial({
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  fragmentShader,
+  side: THREE.DoubleSide,
+  transparent: false,
+  uniforms: {
+    instColor: {
+      value: new THREE.Vector3(0.92, 0.84, 0.72),
+    },
+    Texture: {
+      value: saturnTexture,
+    },
+  },
+});
+const planet = new THREE.Mesh(saturnShape, saturnMaterial);
+
+const ringGeometry = new THREE.RingGeometry(80, 150, 180);
+
+const posGeom = ringGeometry.attributes.position;
+
+const v3Geom = new THREE.Vector3();
+
+for (let i = 0; i < posGeom.count; i++) {
+  v3Geom.fromBufferAttribute(posGeom, i);
+  ringGeometry.attributes.uv.setXY(i, v3Geom.length() < 119 ? 0 : 1, 1);
+}
+const ringTex = new THREE.TextureLoader().load(
+  "../../assets/Texture/saturn_ring.png"
+);
+
+const ringMaterial = new THREE.MeshBasicMaterial({
+  map: ringTex,
+  color: 0xffffff,
+  side: THREE.DoubleSide,
+  transparent: true,
+});
+
+const ring = new THREE.Mesh(ringGeometry, ringMaterial);
+ring.rotation.x = 80;
+
+// Shape of uranus
+const uranusShape = new THREE.SphereGeometry(50, 65, 65);
+//uranus Texture
+const uranusTexture = new THREE.TextureLoader().load(
+  "../../assets/Texture/uranus.jpg"
+);
+// uranus Material
+const uranusMaterial = new CustomShaderMaterial({
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    instColor: {
+      value: new THREE.Vector3(0.74, 0.92, 0.9),
+    },
+    Texture: {
+      value: uranusTexture,
+    },
+  },
+});
+// Shape of neptune
+const neptuneShape = new THREE.SphereGeometry(49.5, 60, 60);
+//neptune Texture
+const neptuneTexture = new THREE.TextureLoader().load(
+  "../../assets/Texture/neptune.jpg"
+);
+// neptune Material
+const neptuneMaterial = new CustomShaderMaterial({
+  baseMaterial: THREE.MeshPhysicalMaterial,
+  vertexShader,
+  fragmentShader,
+  uniforms: {
+    instColor: {
+      value: new THREE.Vector3(0.52, 0.68, 0.863),
+    },
+    Texture: {
+      value: neptuneTexture,
+    },
+  },
+});
+//Mesh all together
+export const mercury = new THREE.Mesh(mercuryShape, mercuryMaterial);
+export const venus = new THREE.Mesh(venusShape, venusMaterial);
+export const earth = new THREE.Mesh(earthShape, earthMaterial);
+export const mars = new THREE.Mesh(marsShape, marsMaterial);
+export const jupiter = new THREE.Mesh(jupiterShape, jupiterMaterial);
+export const saturn = new THREE.Group();
+export const uranus = new THREE.Mesh(uranusShape, uranusMaterial);
+export const neptune = new THREE.Mesh(neptuneShape, neptuneMaterial);
+saturn.add(planet, ring);
