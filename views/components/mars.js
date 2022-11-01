f58be484ca1bf103b74a528f81436b706ed44be2
Santiago <igosantiago91@gmail.com>
2022-11-01 15:25:10 +0700

    naming file

--- views/components/mars.js
+++ views/components/mars.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import CustomShaderMaterial from "three-custom-shader-material/vanilla";
 //Import Shaders
-import vertexShader from "../shaders/mercury/vertex.glsl";
+import vertexShader from "../shaders/mars/vertex.glsl";
 import fragmentShader from "../shaders/mars/fragment.glsl";
 // Shape of mars
 const marsShape = new THREE.SphereGeometry(35, 105, 30);
