5619380a9a0e9fd16dc91ea56d163f70b9caa121
Santiago <igosantiago91@gmail.com>
2022-11-28 12:29:24 +0700

    animate

--- views/JS/Planets.js
+++ views/JS/Planets.js
@@ -13,6 +13,9 @@ import { loadingManager } from "../utils/LoadingManager";
 import Stats from "stats.js";
 
 const stats = new Stats();
+let isClick = false;
+const back = document.getElementById("back");
+
 stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom);
 //Import all components
@@ -133,32 +136,32 @@ function animate() {
 
 // click event on plantets mesh
 sun.addEventListener("click", (event) => {
-  plOnClick(sun, 900, 50, 250, true);
+  plOnClick(sun, 900, 50, 250);
 });
 mercury.addEventListener("click", (event) => {
-  plOnClick(mercury, 300, 50, 100, true);
+  plOnClick(mercury, 300, 50, 100);
 });
 
 venus.addEventListener("click", (event) => {
-  plOnClick(venus, 150, 50, 80, true);
+  plOnClick(venus, 150, 50, 80);
 });
 earth.addEventListener("click", (event) => {
-  plOnClick(earth, 180, 50, 80, true);
+  plOnClick(earth, 180, 50, 80);
 });
 mars.addEventListener("click", (event) => {
-  plOnClick(mars, -200, 40, 30, true);
+  plOnClick(mars, -200, 40, 30);
 });
 jupiter.addEventListener("click", (event) => {
-  plOnClick(jupiter, -250, 40, 50, true);
+  plOnClick(jupiter, -250, 40, 50);
 });
 saturn.addEventListener("click", (event) => {
-  plOnClick(saturn, -280, 40, 50, true);
+  plOnClick(saturn, -280, 40, 50);
 });
 uranus.addEventListener("click", (event) => {
-  plOnClick(uranus, -200, 40, 10, true);
+  plOnClick(uranus, -200, 40, 10);
 });
 neptune.addEventListener("click", (event) => {
-  plOnClick(neptune, -200, 40, 10, true);
+  plOnClick(neptune, -200, 40, 10);
 });
 animate();
 
@@ -183,10 +186,13 @@ function onTransitionEnd(event) {
 function onShow() {
   infoId.classList.remove("hidden");
 }
+function onHide() {
+  infoId.classList.add("hidden");
+}
 
-function plOnClick(planets, x, y, z, isUseCoor) {
+function plOnClick(planets, x, y, z) {
   let plCoor = coorObj(planets);
-  if (isUseCoor == true) {
+  if (isClick == false) {
     gsap.to(camera.position, {
       x: plCoor.x - x,
       duration: 3,
@@ -200,19 +206,25 @@ function plOnClick(planets, x, y, z, isUseCoor) {
       z: plCoor.z - z,
       duration: 3,
     });
-  } else {
+    isClick = true;
+  }
+}
+back.addEventListener("click", backTo);
+function backTo() {
+  if (isClick == true) {
+    onHide();
     gsap.to(camera.position, {
-      x: -x,
-      duration: 3,
-      onComplete: onShow,
-    });
-    gsap.to(camera.position, {
-      y: +y,
-      duration: 3,
-    });
-    gsap.to(camera.position, {
-      z: -z,
+      x: -373.55482345690655,
       duration: 3,
     });
+    gsap.to(camera.position, {
+      y: 704.7150454413226,
+      duration: 3,
+    });
+    gsap.to(camera.position, {
+      z: 1411.0537416811678,
+      duration: 3,
+    });
+    isClick = false;
   }
 }
